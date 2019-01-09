6ce75dbb51ee70ff8ff9c1f71ddadffc4ae3204b
like218726 <like218726@vip.qq.com>
2019-01-09 18:37:49 +0800

    更新省市区逻辑

--- Addr_select/code/application/admin/controller/Xxx.php
+++ Addr_select/code/application/admin/controller/Xxx.php
@@ -2,8 +2,10 @@
 namespace app\admin\controller;
 class Xxx extends Base {
 	public function test(){
+		//方法一:
 		$address_select = $this->address_select();
-		$this->assign('addr_select',$address_select);  		
+		$this->assign('addr_select',$address_select); 
+		//方法二: 直接用模板代码 		
 	}	
 }	
 ?>
