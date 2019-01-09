645660d5b38e3def36a0836daa4484c66e198a5e
like218726 <like218726@vip.qq.com>
2019-01-09 18:01:37 +0800

    修改省市区代码

--- Addr_select/code/application/admin/controller/Xxx.php
+++ Addr_select/code/application/admin/controller/Xxx.php
@@ -3,7 +3,7 @@ namespace app\admin\controller;
 class Xxx extends Base {
 	public function test(){
 		$address_select = $this->address_select();
-		$this->assign('address_select',$address_select);  		
+		$this->assign('addr_select',$address_select);  		
 	}	
 }	
 ?>
